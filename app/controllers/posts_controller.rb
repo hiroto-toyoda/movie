34977bb53535917998f8e6598793e124e4bd65d1
toyoda-hiroto <to022323@gmail.comm>
2020-04-20 01:07:58 +0900

    投稿詳細ページ作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,9 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order('created_at DESC')
   end
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
 
   private
     def post_params
